304ff766258f54397eb5d9f204e1dea1e6a4b409
thebladehit <boqdanyarmolka@gmail.com>
2023-06-12 00:05:42 +0300

    Add copy button.

--- src/frontend/src/CodeDisplay.js
+++ src/frontend/src/CodeDisplay.js
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism-tomorrow.css';
 
 const CodeDisplay = ({ code }) => {
+  const copyCode = () => {
+    navigator.clipboard.writeText(code);
+    setCopy('Copied');
+    setTimeout(() => {setCopy('Copy')}, 1000);
+  }
+
+  const [disabled, setDisabled] = useState('disabled');
+  const [copy, setCopy] = useState('Copy');
+
   useEffect(() => {
+    code === 'your page will be here' ? setDisabled('disabled') : setDisabled('');
     Prism.highlightAll();
   }, [code]);
 
@@ -12,6 +22,7 @@ const CodeDisplay = ({ code }) => {
       <pre>
         <code className="language-html">{code}</code>
       </pre>
+      <button className={`copy ${disabled}`} onClick={copyCode}>{copy}</button>
     </div>
   );
 };
