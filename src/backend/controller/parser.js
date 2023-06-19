29d49ba09c8a7edad8041632e878561000d42006
thebladehit <boqdanyarmolka@gmail.com>
2023-06-19 17:12:21 +0300

    Change server part (GET -> POST).

--- src/backend/controller/parser.js
+++ src/backend/controller/parser.js
@@ -1,16 +1,28 @@
 import parser from "../service/parser.js";
 import fs from 'fs';
 
-export default async (request, response) => {
+export default (request, response) => {
   try {
-    const data = await parser(request.headers['parseurl']);
-    response.writeHead(200);
-    console.log('this');
-    response.end(JSON.stringify({ data }));
-    await fs.promises.writeFile('data.txt', data);
+    let body = '';
+    request.on('data', chunk => {
+      body += chunk;
+    });
+
+    request.on('end', async () => {
+      try {
+        const data = await parser(JSON.parse(body));
+        response.writeHead(200);
+        response.end(JSON.stringify({ data }));
+        await fs.promises.writeFile('data.txt', data);
+      } catch (err) {
+        response.writeHead(400);
+        response.end(JSON.stringify({ err: 'Invalid url' }));
+        console.log(err);
+      }
+    });
   } catch (err) {
-    response.writeHead(400);
-    response.end(JSON.stringify({ err: 'Invalid url' }));
+    response.writeHead(500);
+    response.end(JSON.stringify({ err: 'Server eror' }));
     console.log(err);
   }
 }
--- src/backend/service/parser.js
+++ src/backend/service/parser.js
@@ -1,10 +1,10 @@
 import { JSDOM } from 'jsdom';
 import axios from 'axios';
 
-export default async (url) => {
-  console.log(`Get ${url}`); 
+export default async (body) => {
+  console.log(`Get ${body}`); 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(body.formData.url);
     const dom = new JSDOM(response.data);
     const serializedDom = dom.serialize();
     return serializedDom;
