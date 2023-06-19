875b48c6e30f2da0a79607670a6ece11e50cc657
thebladehit <boqdanyarmolka@gmail.com>
2023-06-19 14:25:04 +0300

    Remove flex-block container.

--- src/frontend/src/Fill.js
+++ src/frontend/src/Fill.js
@@ -6,11 +6,11 @@ const Fill = ({ formData, setFormData, fields }) => {
   };
 
   return (
-    <div className="flex-block">
+    <>
       <div><input type="text" name="url" value={formData.url} onChange={handleChange} placeholder='http://' className={`${fields.url}`} /></div>
       <div><input type="text" name="element" value={formData.element} onChange={handleChange} placeholder='class=...' className={`${fields.element}`} /></div>
       <button type="submit">Get Page Html</button>
-    </div>
+    </>
   );
 }
 
