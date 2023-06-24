189f3cef9eeedb9cc0b2fef36d643617289e7376
thebladehit <boqdanyarmolka@gmail.com>
2023-06-24 17:36:51 +0300

    Add parsing by link.

--- src/backend/service/parser.js
+++ src/backend/service/parser.js
@@ -13,9 +13,23 @@ const parseByType = {
     const elements = dom.window.document.querySelectorAll(body.formData.element);
     if (elements.length === 0) throw new Error('There is not such class, id or element');
     let res = '';
-    elements.forEach(elem => res += `${elem.textContent}\n`);
+    elements.forEach(elem => res += `${body.formData.tagName === '-' ? elem.textContent : elem[body.formData.tagName]}\n`);
     return res;
   },
+  dynamicPages: async (body) => {
+    console.log(body.formData.range);
+    const urlsArray = body.formData.range.split(body.formData.range.includes('\n') ? '\n' : ' ');
+    let res = '';
+    for (const url of urlsArray) {
+      console.log(`${body.formData.url}${url}`);
+      const response = await doQuery(`${body.formData.url}${url}`);
+      const dom = convertToDom(response.data);
+      const elements = dom.window.document.querySelectorAll(body.formData.element);
+      if (elements.length === 0) throw new Error('There is not such class, id or element');
+      elements.forEach(elem => res += `${body.formData.tagName === '-' ? elem.textContent : elem[body.formData.tagName]}\n`);
+    }
+    return res;
+  }
 };
 
 const doQuery = async (url) => {
--- src/frontend/src/MainPage/form/Fill.js
+++ src/frontend/src/MainPage/form/Fill.js
@@ -13,7 +13,7 @@ const Fill = ({ formData, setFormData, fields }) => {
       <div><input type="text" name="url" value={formData.url} onChange={handleChange} placeholder='http://' className={`${fields.url}`} /></div>
       <div><input type="text" name="element" value={formData.element} onChange={handleChange} placeholder='class= e.g .some-class' className={`${fields.element}`} /></div>
       <div><input type="text" name="tagName" value={formData.tagName} onChange={handleChange} placeholder='tag name e.g a or p' className={`${fields.tagName}`} /></div>
-      <div><input type="text" name="range" value={formData.range} onChange={handleChange} placeholder='range e.g [some, something] or 1-10' className={`${fields.range}`} /></div>
+      <div><input type="text" name="range" value={formData.range} onChange={handleChange} placeholder='range e.g /some, /something' className={`${fields.range}`} /></div>
       <button type="submit">Get Page Html</button>
     </>
   );
