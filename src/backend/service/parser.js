4476f237e80a41725e289610acb501be9f91a6c5
thebladehit <boqdanyarmolka@gmail.com>
2023-06-27 22:40:01 +0300

    Add caching urls.

--- src/backend/service/parser.js
+++ src/backend/service/parser.js
@@ -1,15 +1,15 @@
 import { JSDOM } from 'jsdom';
 import axios from 'axios';
 
+const cachedUrl = {};
+
 const parseByType = {
   gHTML: async (body) => {
-    const response = await doQuery(body.formData.url);
-    const dom = convertToDom(response.data);
+    const dom = await getDom(body.formData.url);
     return dom.serialize(); 
   },
   static: async (body) => {
-    const response = await doQuery(body.formData.url);
-    const dom = convertToDom(response.data);
+    const dom = await getDom(body.formData.url);
     const elements = dom.window.document.querySelectorAll(body.formData.element);
     if (elements.length === 0) throw new Error('There is not such class, id or element');
     let res = '';
@@ -17,13 +17,11 @@ const parseByType = {
     return res;
   },
   dynamicPages: async (body) => {
-    console.log(body.formData.range);
     const urlsArray = body.formData.range.split(body.formData.range.includes('\n') ? '\n' : ' ');
     let res = '';
     for (const url of urlsArray) {
       console.log(`${body.formData.url}${url}`);
-      const response = await doQuery(`${body.formData.url}${url}`);
-      const dom = convertToDom(response.data);
+      const dom = await getDom(`${body.formData.url}${url}`);
       const elements = dom.window.document.querySelectorAll(body.formData.element);
       if (elements.length === 0) throw new Error('There is not such class, id or element');
       elements.forEach(elem => res += `${body.formData.tagName === '-' ? elem.textContent : elem[body.formData.tagName]}\n`);
@@ -39,7 +37,44 @@ const doQuery = async (url) => {
     throw new Error('Invalid Url');
   }
 };
+
 const convertToDom = (data) => new JSDOM(data);
+
+const getDom = async (url) => {
+  if (cachedUrl[url]) {
+    console.log('From cache');
+    const obj = cachedUrl[url];
+    obj.used++;
+    return obj.data;
+  } else {
+    const response = await doQuery(url);
+    const dom = convertToDom(response.data);
+    cache(url, dom);
+    return dom;
+  }
+}
+
+const cache = (url, data) => {
+  if (!cachedUrl.hasOwnProperty(url)) {
+    cachedUrl[url] = {
+      data,
+      used: 1
+    };
+    clearCache();
+  }
+};
+
+const clearCache = () => {
+  if (Object.keys(cachedUrl).length > 10) {
+    let curUrl;
+    for (const key in cachedUrl) {
+      if (!curUrl || cachedUrl[key].used < cachedUrl[curUrl].used) {
+        curUrl = key;
+      }
+    }
+    delete cachedUrl[curUrl];
+  }
+};
 
 export default async (body) => {
   console.log(body);
