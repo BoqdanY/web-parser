a15684515293cc1f326f64ea85a28a5ad8810f87
thebladehit <boqdanyarmolka@gmail.com>
2023-06-27 22:50:38 +0300

    Change infoPage.

--- src/frontend/src/InfoPage/InfoPage.js
+++ src/frontend/src/InfoPage/InfoPage.js
@@ -5,18 +5,14 @@ const InfoPage = () => {
       <h2>How to use it?</h2>
       <p>
         <span className="info-span">1. Explore the website.</span>
-        <span className="info-span">
-          2. Check what type of data collection the website has.
-          <ul>
-            <li>Static</li>
-            <li>Dynamic by getting page <span className="eg-span">(e.g https://someWebSite/page?=1)</span></li>
-            <li>Dynamic by getting data and add it to page <span className="eg-span">(e.g in the Network tab in the console you may find some requests)</span></li>
-          </ul>
-          If nothing are suitable for you, It's parser is not for you :(
-        </span>
-        <span className="info-span">3. Explore the HTML markup of the website <span className="eg-span">(find the #id or .clasName of element which contain information which you want to get).</span></span>
+        <span className="info-span">2. Explore the HTML markup of the website <span className="eg-span">(find the #id or .clasName of element which contain information which you want to get).</span></span>
+        <span className="info-span">3. If you want to get data from tag - write name of tag.</span>
         <span className="info-span">4. Fill the form and push the button.</span>
       </p>
+      <h2>Warnings</h2>
+      <span className="info-span">In order to get <span className="eg-span">element by class</span> - write .class-name in second input.</span>
+      <span className="info-span">In order to get <span className="eg-span">element by id</span> - write #id-name in second input.</span>
+      <span className="info-span">In order to get <span className="eg-span">text of element</span> - write <span className="eg-span">"-"</span> in third input.</span>
     </>
   );
 };
